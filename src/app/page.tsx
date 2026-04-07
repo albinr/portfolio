0288e9d0a5f9afa3464e267f1cc00b2195f1c915
albinr <albin@rybergs.net>
2026-04-07 20:06:02 +0200

    update: added new image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,6 +13,7 @@ export default function Home() {
               { src: "/images/me-2.jpg", alt: "Albin Ryberg 2" },
               { src: "/images/me-3.jpg", alt: "Albin Ryberg 3" },
               { src: "/images/me-4.jpg", alt: "Albin Ryberg 4" },
+              { src: "/images/me-5.jpg", alt: "Albin Ryberg 5" },
             ]}
           />
         </div>
